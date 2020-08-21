514325df4872d76f7bd49a785936977ed2f2c79e
Milton <A01634505@itesm.mx>
2020-08-21 12:54:05 -0500

    Cookie implementation for signing in and out

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const usersRepo = require('./repos/users');
+const cookieSession = require('cookie-session');
+const usersRepo = require('./repositories/users');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+	cookieSession({
+		keys: [ 'drs5645thg43dfghfhfg3321a' ]
+	})
+);
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
 	res.send(`
-    <div>
+	<div>
+		Hello ${req.session.userId}!
         <form method="POST">
             <input name="email" placeholder="email"/>
             <input name="password" placeholder="Password"/>
@@ -19,7 +26,7 @@ app.get('/', (req, res) => {
     `);
 });
 
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
 	const { email, password, passwordConf } = req.body;
 
 	const existingUser = await usersRepo.getOneBy({ email });
@@ -32,7 +39,46 @@ app.post('/', async (req, res) => {
 		return res.send('Passwords must match');
 	}
 
+	const user = await usersRepo.create({ email, password });
+
+	req.session.userId = user.id;
+
 	res.send('Account created');
+});
+
+app.get('/signin', (req, res) => {
+	res.send(`
+	<div>
+        <form method="POST">
+            <input name="email" placeholder="email"/>
+            <input name="password" placeholder="Password"/>
+            <button>Sign in</button>
+        </form>
+    </div>
+    `);
+});
+
+app.post('/signin', async (req, res) => {
+	const { email, password } = req.body;
+
+	const user = await usersRepo.getOneBy({ email });
+
+	if (!user) {
+		return res.send('Email not found');
+	}
+
+	if (user.password !== password) {
+		return res.send('Invalid password');
+	}
+
+	req.session.userId = user.id;
+
+	res.send('You are logged in!');
+});
+
+app.get('/signout', (req, res) => {
+	req.session = null;
+	res.send('Logged out');
 });
 
 app.listen(3000, () => {
