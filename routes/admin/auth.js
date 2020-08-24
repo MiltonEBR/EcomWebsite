1abd2e6a3d3105551d6abebb58f055a34c65578d
Milton <A01634505@itesm.mx>
2020-08-24 15:31:30 -0500

    Refactored signing validation

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -9,25 +11,34 @@ router.get('/signup', (req, res) => {
 	res.send(signupTemplate({ req }));
 });
 
-router.post('/signup', async (req, res) => {
-	const { email, password, passwordConf } = req.body;
+router.post(
+	'/signup',
+	[
+		check('email').trim().normalizeEmail().isEmail().custom(async (email) => {
+			const existingUser = await usersRepo.getOneBy({ email });
+			if (existingUser) {
+				throw new Error('Email in use');
+			}
+		}),
+		check('password').trim().isLength({ min: 4, max: 20 }),
+		check('passwordConf').trim().isLength({ min: 4, max: 20 }).custom((passwordConf, { req }) => {
+			if (req.body.password !== passwordConf) {
+				throw new Error('Passwords must match');
+			}
+		})
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		console.log(errors);
 
-	const existingUser = await usersRepo.getOneBy({ email });
+		const { email, password, passwordConf } = req.body;
+		const user = await usersRepo.create({ email, password });
 
-	if (existingUser) {
-		return res.send('Email in use');
+		req.session.userId = user.id;
+
+		res.send('Account created');
 	}
-
-	if (password !== passwordConf) {
-		return res.send('Passwords must match');
-	}
-
-	const user = await usersRepo.create({ email, password });
-
-	req.session.userId = user.id;
-
-	res.send('Account created');
-});
+);
 
 router.get('/signin', (req, res) => {
 	res.send(signinTemplate());
